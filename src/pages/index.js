97bca1bc3bacc17543173772cb636e63542b9f0d
Mads Schou Olesen <mschouolesen@gmail.com>
2023-05-11 10:52:25 +0200

    personalinfo skelet

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import Header from "@/components/header";
+import Header from "@/Components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 

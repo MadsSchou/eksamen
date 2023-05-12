b12b93a0ae121af2f1b1e96472f207c4684d1ed9
Mads Schou Olesen <mschouolesen@gmail.com>
2023-05-12 11:53:05 +0200

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,87 +16,41 @@ export default function Home() {
       <Header />
 
       <div style={{ position: "relative" }}>
-        <Image
-          src="/assets/baggrund.jpg"
-          alt="Background image"
-          width={800}
-          height={600}
-          layout="responsive"
-        />
+        <Image src="/assets/baggrund.jpg" alt="Background image" width={800} height={600} layout="responsive" />
 
-        <div
-          className={styles.container}
-          style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}
-        >
-          <h1
-            className={styles.headline}
-            style={{ position: "relative", zIndex: 1 }}
-          >
+        <div className={styles.container} style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}>
+          <h1 className={styles.headline} style={{ position: "relative", zIndex: 1 }}>
             Thunderstrike Metal Festival
           </h1>
 
-          <h2
-            className={styles.center}
-            style={{ position: "relative", zIndex: 1 }}
-          >
-            Thunderstruck Music Festival is a seven-day event that brings
-            together some of the best heavy metal bands from around the world.
-            Held in a sprawling outdoor venue, the festival boasts multiple
-            stages that feature non-stop performances from morning until night.
+          <h2 className={styles.center} style={{ position: "relative", zIndex: 1 }}>
+            Thunderstruck Music Festival is a seven-day event that brings together some of the best heavy metal bands from around the world. Held in a sprawling outdoor venue, the festival boasts multiple stages that feature non-stop performances from morning until night.
           </h2>
 
           <div className={styles.buttons}>
-            <button className={styles.button}>View Schedule</button>
+            <a href="/schedule" className={styles.button}>
+              View Schedule
+            </a>
             <button className={styles.button}>Buy tickets</button>
           </div>
 
           <div className={styles.cardContainer}>
             <div className={styles.card}>
-              <Image
-                src="/assets/placeholder.jpg"
-                alt="Card image"
-                width={300}
-                height={200}
-                layout="responsive"
-              />
+              <Image src="/assets/placeholder.jpg" alt="Card image" width={300} height={200} layout="responsive" />
               <h3 className={styles.cardTitle}>Band Name 1</h3>
-              <p className={styles.cardText}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut
-                eleifend augue. Ut eget metus ac nibh accumsan pulvinar. Donec
-                semper mi id augue tristique congue. Ut eu urna purus.{" "}
-              </p>
+              <p className={styles.cardText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut eleifend augue. Ut eget metus ac nibh accumsan pulvinar. Donec semper mi id augue tristique congue. Ut eu urna purus. </p>
             </div>
 
             <div className={styles.card}>
-              <Image
-                src="/assets/placeholder.jpg"
-                alt="Card image"
-                width={300}
-                height={200}
-                layout="responsive"
-              />
+              <Image src="/assets/placeholder.jpg" alt="Card image" width={300} height={200} layout="responsive" />
               <h3 className={styles.cardTitle}>Band Name 2</h3>
-              <p className={styles.cardText}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut
-                eleifend augue. Ut eget metus ac nibh accumsan pulvinar. Donec
-                semper mi id augue tristique congue. Ut eu urna purus.{" "}
-              </p>
+              <p className={styles.cardText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut eleifend augue. Ut eget metus ac nibh accumsan pulvinar. Donec semper mi id augue tristique congue. Ut eu urna purus. </p>
             </div>
 
             <div className={styles.card}>
-              <Image
-                src="/assets/placeholder.jpg"
-                alt="Card image"
-                width={300}
-                height={200}
-                layout="responsive"
-              />
+              <Image src="/assets/placeholder.jpg" alt="Card image" width={300} height={200} layout="responsive" />
               <h3 className={styles.cardTitle}>Band Name 3</h3>
-              <p className={styles.cardText}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut
-                eleifend augue. Ut eget metus ac nibh accumsan pulvinar. Donec
-                semper mi id augue tristique congue. Ut eu urna purus.{" "}
-              </p>
+              <p className={styles.cardText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut eleifend augue. Ut eget metus ac nibh accumsan pulvinar. Donec semper mi id augue tristique congue. Ut eu urna purus. </p>
             </div>
           </div>
         </div>

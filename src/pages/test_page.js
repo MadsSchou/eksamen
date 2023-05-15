3f4fc0f05b369641f6a73d4e0603dcaf1b0bc9cd
Daniel Nygaard <dani12h1@stud.kea.dk>
2023-05-15 12:06:30 +0200

    Steps done

---
+++ src/pages/test_page.js
@@ -0,0 +1,13 @@
+import Steps_flow from "@/components/steps";
+import Steps from "@/components/steps";
+import React from "react";
+
+function test_page() {
+  return (
+    <div>
+      <Steps_flow />
+    </div>
+  );
+}
+
+export default test_page;

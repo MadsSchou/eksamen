319a38f5f07fedd2ff83d913e75d08556506603a
Christine Ramm <chri42mp@stud.kea.dk>
2023-05-22 12:51:21 +0200

    favorite toggle everything + bit of styling

--- src/pages/scheduledates.js
+++ src/pages/scheduledates.js
@@ -39,6 +39,12 @@ function App() {
     });
   };
 
+  const [favorite, setFavorite] = useState(false);
+
+  const handleFavoriteClick = () => {
+    setFavorite(!favorite);
+  };
+
   const renderTimeSlots = (timeSlots) => {
     return (
       <div className={styles["time-slots"]}>
@@ -56,9 +62,7 @@ function App() {
                 <div
                   className={styles["time-range"]}
                 >{`${timeSlot.start} - ${timeSlot.end}`}</div>
-                <div className={styles["act-details"]}>
-                  <p>{timeSlot.act}</p>
-                </div>
+                <div className={styles["act-details"]}></div>
                 <div className={styles["break-indicator"]}>Break</div>
               </div>
             );
@@ -69,21 +73,35 @@ function App() {
               key={`${timeSlot.start}-${timeSlot.end}`}
               className={styles["time-slot"]}
             >
+              <div className={styles["svg-container"]}>
+                <svg
+                  className={`${styles.favorite} ${
+                    favorite ? styles.active : ""
+                  }`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill={favorite ? "red" : "white"}
+                  viewBox="0 0 16 16"
+                  onClick={handleFavoriteClick}
+                >
+                  <path d="M6 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H9v1.07a7.001 7.001 0 0 1 3.274 12.474l.601.602a.5.5 0 0 1-.707.708l-.746-.746A6.97 6.97 0 0 1 8 16a6.97 6.97 0 0 1-3.422-.892l-.746.746a.5.5 0 0 1-.707-.708l.602-.602A7.001 7.001 0 0 1 7 2.07V1h-.5A.5.5 0 0 1 6 .5zm2.5 5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5zM.86 5.387A2.5 2.5 0 1 1 4.387 1.86 8.035 8.035 0 0 0 .86 5.387zM11.613 1.86a2.5 2.5 0 1 1 3.527 3.527 8.035 8.035 0 0 0-3.527-3.527z" />
+                </svg>
+              </div>
               <div
                 className={styles["time-range"]}
               >{`${timeSlot.start} - ${timeSlot.end}`}</div>
               <div className={styles["act-details"]}>
-                <p>{timeSlot.act}</p>
                 <div>
                   <img
-                    width={"100px"}
-                    height={"100px"}
+                    width={"100%"}
                     src={
                       imgForBand[0]?.logo ||
                       imgForBand[0]?.logoCredits.split(", ")[2]
                     }
                     loading="lazy"
                   />
+                  <p>{timeSlot.act}</p>
                 </div>
               </div>
             </div>
@@ -93,7 +111,7 @@ function App() {
     );
   };
   return (
-    <div>
+    <div className={styles.contentSchedule}>
       <select onChange={(e) => chooseStage(e.target.value)}>
         <option value="Midgard">Vælg stage</option>
         {Object.entries(schedule).map(([stage]) => (

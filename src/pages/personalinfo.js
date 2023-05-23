69df0edb9044dc87bace46a008cd95660e8ef954
Mads Schou Olesen <mschouolesen@gmail.com>
2023-05-23 16:52:55 +0200

    p

--- src/pages/personalinfo.js
+++ src/pages/personalinfo.js
@@ -1,23 +1,36 @@
-import Head from "next/head";
-import { Inter } from "next/font/google";
-import PersonalInfo from "../Components/PersonalInfo";
-import styles from "./personalinfo.module.css";
-import Flow from "@/Components/steps";
+// import Head from "next/head";
+// import { Inter } from "next/font/google";
+// import PersonalInfo from "../Components/PersonalInfo";
+// import styles from "./personalinfo.module.css";
+// import Flow from "@/Components/steps";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+// export default function Home() {
+//   return (
+//     <>
+//       <Head>
+//         <title>Thunderstrike Metal Festival</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+//       <Flow />
+
+//       <div>
+//         <PersonalInfo />
+//       </div>
+//     </>
+//   );
+// }
+import React from "react";
+import MultiStepForm from "@/Components/MultiStepForm";
+
+const HomePage = () => {
   return (
-    <>
-      <Head>
-        <title>Thunderstrike Metal Festival</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Flow />
-
-      <div>
-        <PersonalInfo />
-      </div>
-    </>
+    <div>
+      <h1>Multi-Step Form Example</h1>
+      <MultiStepForm />
+    </div>
   );
-}
+};
+
+export default HomePage;

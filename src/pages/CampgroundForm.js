f6eacee4b67939ea0640ab57ad72c6abf0edc300
Mads Schou Olesen <mschouolesen@gmail.com>
2023-05-23 23:22:36 +0200

    vercel2

--- src/pages/CampgroundForm.js
+++ src/pages/CampgroundForm.js
@@ -2,8 +2,8 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import Link from "next/link";
 import styles from "./CampgroundForm.module.js";
-import Ordreoversigt from "../Components/ordreoversigt/ordreoversigt";
-import Flow from "@/Components/steps";
+import Ordreoversigt from "../components/ordreoversigt/ordreoversigt";
+import Flow from "@/components/steps";
 
 const inter = Inter({ subsets: ["latin"] });
 

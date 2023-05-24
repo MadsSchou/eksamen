3c45d56e6f0c5ab7c7dcb9735f052bd2d89aa446
Christine Ramm <chri42mp@stud.kea.dk>
2023-05-24 16:59:43 +0200

    favicon kinda works

--- src/pages/scheduledates.js
+++ src/pages/scheduledates.js
@@ -1,6 +1,8 @@
 import { useContext, useEffect, useState } from "react";
 import styles from "./scheduledates.module.css";
 import { imgContext } from "@/context/ImgContext";
+import FavIcon from "@/components/FavIcon";
+import { auth, db } from "@/firebase";
 
 function App() {
   const [schedule, setSchedule] = useState([]);
@@ -16,14 +18,19 @@ function App() {
     );
     setChosenSchedule(chosenSchedule);
   }
-
+  useEffect(() => {
+    console.log(auth.currentUser);
+    if (auth.currentUser) {
+      db.collection(auth.currentUser.uid)
+        .get()
+        .then((data) => console.log(data));
+    }
+  }, []);
   useEffect(() => {
     fetch("https://charm-pale-tub.glitch.me/schedule")
       .then((response) => response.json())
       .then((data) => {
         setSchedule(data);
-
-        console.log(data);
       })
       .catch((error) => {
         console.log(error);
@@ -45,12 +52,6 @@ function App() {
         </div>
       );
     });
-  };
-
-  const [favorite, setFavorite] = useState(false);
-
-  const handleFavoriteClick = () => {
-    setFavorite(!favorite);
   };
 
   const renderTimeSlots = (timeSlots) => {
@@ -81,21 +82,7 @@ function App() {
               key={`${timeSlot.start}-${timeSlot.end}`}
               className={styles["time-slot"]}
             >
-              <div className={styles["svg-container"]}>
-                <svg
-                  className={`${styles.favorite} ${
-                    favorite ? styles.active : ""
-                  }`}
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill={favorite ? "red" : "white"}
-                  viewBox="0 0 16 16"
-                  onClick={handleFavoriteClick}
-                >
-                  <path d="M6 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H9v1.07a7.001 7.001 0 0 1 3.274 12.474l.601.602a.5.5 0 0 1-.707.708l-.746-.746A6.97 6.97 0 0 1 8 16a6.97 6.97 0 0 1-3.422-.892l-.746.746a.5.5 0 0 1-.707-.708l.602-.602A7.001 7.001 0 0 1 7 2.07V1h-.5A.5.5 0 0 1 6 .5zm2.5 5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5zM.86 5.387A2.5 2.5 0 1 1 4.387 1.86 8.035 8.035 0 0 0 .86 5.387zM11.613 1.86a2.5 2.5 0 1 1 3.527 3.527 8.035 8.035 0 0 0-3.527-3.527z" />
-                </svg>
-              </div>
+              <FavIcon data={timeSlot} stage={stages} />
               <div
                 className={styles["time-range"]}
               >{`${timeSlot.start} - ${timeSlot.end}`}</div>
@@ -103,6 +90,7 @@ function App() {
                 <div>
                   <img
                     width={"100%"}
+                    height={"150px"}
                     src={
                       imgForBand[0]?.logo ||
                       imgForBand[0]?.logoCredits.split(", ")[2]

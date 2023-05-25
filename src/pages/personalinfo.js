e5f83b0ff52a3221a1210a18dc009edfb1ac1475
Mads Schou Olesen <mschouolesen@gmail.com>
2023-05-25 09:40:33 +0200

    .

--- src/pages/personalinfo.js
+++ src/pages/personalinfo.js
@@ -1,31 +1,7 @@
-// import Head from "next/head";
-// import { Inter } from "next/font/google";
-// import PersonalInfo from "../Components/PersonalInfo";
-// import styles from "./personalinfo.module.css";
-// import Flow from "@/Components/steps";
-
-// const inter = Inter({ subsets: ["latin"] });
-
-// export default function Home() {
-//   return (
-//     <>
-//       <Head>
-//         <title>Thunderstrike Metal Festival</title>
-//         <link rel="icon" href="/favicon.ico" />
-//       </Head>
-//       <Flow />
-
-//       <div>
-//         <PersonalInfo />
-//       </div>
-//     </>
-//   );
-// }
 import React from "react";
 import MultiStepForm from "@/components/multistep/MultiStepForm";
 import Head from "next/head";
-import Flow from "@/Components/steps";
-// import { Inter } from "next/font/google";
+import Flow from "@/components/steps";
 
 const HomePage = () => {
   return (

c7e7ba4102d75f316c9b5a1bf6b0a55c19056df8
Christine Ramm <chri42mp@stud.kea.dk>
2023-05-25 13:08:23 +0200

    styling on login, linking in nav

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -9,11 +9,11 @@ import { useAuth } from "@/context/AuthContext";
 
 const MENU_LIST = [
   {
-    text: "Home",
-    href: "/",
+    text: "Tidsplan",
+    href: "/scheduledates",
   },
   {
-    text: "Program",
+    text: "Bands",
     href: "/schedule",
   },
   {
@@ -66,7 +66,11 @@ function Navbar() {
             >
               {menu.isButton ? (
                 <Link href={menu.href}>
-                  <button onClick={() => console.log(`${menu.text} button clicked`)}>{menu.text}</button>
+                  <button
+                    onClick={() => console.log(`${menu.text} button clicked`)}
+                  >
+                    {menu.text}
+                  </button>
                 </Link>
               ) : (
                 <NavItem active={activeIdx === idx} {...menu} />
@@ -76,13 +80,27 @@ function Navbar() {
         </div>
         {currentUser ? (
           <Link href={"/dashboard"}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-person-fill"
+              viewBox="0 0 16 16"
+            >
               <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
             </svg>
           </Link>
         ) : (
           <Link href={"/signUp"}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-person-fill"
+              viewBox="0 0 16 16"
+            >
               <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
             </svg>
           </Link>
--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -1,8 +1,8 @@
 import { useAuth } from "@/context/AuthContext";
 import { auth, db } from "@/firebase";
-import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
+import styles from "./signUp.module.css";
 
 export default function signUp() {
   const { login, signUp } = useAuth();
@@ -35,50 +35,69 @@ export default function signUp() {
     }
   }
   return (
-    <div>
-      <div>
-        <h1>Log ind</h1>
-        <form onSubmit={(e) => handleLogIn(e)}>
-          <div>
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" />
-          </div>
-          <button type="submit">Log ind</button>
-        </form>
-      </div>
-      <div>
-        <h1>Opret bruger</h1>
+    <div className={styles["content"]}>
+      <div className={styles["grid_1-1"]}>
+        <div className={styles["login_form"]}>
+          <h1 className={styles["welcome"]}>Velkommen tilbage!</h1>
+          <form onSubmit={(e) => handleLogIn(e)}>
+            <div className={styles[["input-container"]]}>
+              <label className={styles["white"]} htmlFor="email">
+                Email
+              </label>
+              <input type="email" name="email" id="email" placeholder="Email" />
+            </div>
+            <div className={styles[["input-container"]]}>
+              <label className={styles["white"]} htmlFor="password">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+              />
+            </div>
+            <button className={styles["button"]} type="submit">
+              Log ind
+            </button>
+          </form>
+        </div>
 
-        <form onSubmit={(e) => handleSignUp(e)}>
-          <div>
-            <label htmlFor="name">Name: </label>
-            <input type="text" id="name" name="name" />
-          </div>
-          <div>
-            <label htmlFor="email">Email: </label>
-            <input type="email" id="email" name="email" />
-          </div>
-          <div>
-            <label htmlFor="password">Password: </label>
-            <input type="password" id="password" name="password" />
-          </div>
-          <div>
-            <label htmlFor="password_confirmation">
-              Password Confirmation:{" "}
-            </label>
-            <input
-              type="password"
-              id="password_confirmation"
-              name="password_confirmation"
-            />
-          </div>
+        <div className={styles["signUp_form"]}>
+          <form onSubmit={(e) => handleSignUp(e)}>
+            <div className={styles["input-container"]}>
+              <h1 className={styles[["make_user"]]}>Opret bruger</h1>
+              <label htmlFor="name">Name: </label>
+              <input type="text" id="name" name="name" placeholder="Navn" />
+            </div>
+            <div className={styles["input-container"]}>
+              <label htmlFor="email">Email: </label>
+              <input type="email" id="email" name="email" placeholder="Email" />
+            </div>
+            <div className={styles["input-container"]}>
+              <label htmlFor="password">Password: </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+              />
+            </div>
+            <div className={styles["input-container"]}>
+              <label htmlFor="password_confirmation">
+                Password Confirmation:{" "}
+              </label>
+              <input
+                type="password"
+                id="password_confirmation"
+                name="password_confirmation"
+                placeholder="Gentag password"
+              />
+            </div>
 
-          <input type="submit" value={"opret"} />
-        </form>
+            <input className={styles["button"]} type="submit" value={"opret"} />
+          </form>
+        </div>
       </div>
     </div>
   );

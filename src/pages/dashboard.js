2fa7ba725b201cfe1fc883f3c7d9f8e618973a35
Christine Ramm <chri42mp@stud.kea.dk>
2023-05-27 12:50:54 +0200

    no favortites yet, styled

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -48,27 +48,48 @@ export default function Dashboard() {
   if (myFav.length === 0) {
     // Render layout when no favorites are chosen
     return (
-      <div>
-        <h1>Hej {loggedUserDb?.name}</h1>
-        <button
-          onClick={() => {
-            auth.signOut();
-            window.location.href = "/";
-          }}
-        >
-          Log Out
-        </button>
-        <h2>Din tidsplan</h2>
-        <h3>Alt på et sted</h3>
-        <div>line</div>
+      <div className={styles["background_color"]}>
+        <div className={styles["content"]}>
+          <div className={styles["user_container"]}>
+            <div className={styles["user_content"]}>
+              <h1 className={styles["hi_user"]}>Hej {loggedUserDb?.name}</h1>{" "}
+            </div>
+            <button
+              className={styles["logout"]}
+              onClick={() => {
+                auth.signOut();
+                window.location.href = "/";
+              }}
+            >
+              Log Out
+            </button>
+          </div>
 
-        <div className={styles["grid_7x1"]}>
-          {allDay?.map((day) => {
-            return <p>{day}</p>;
-          })}
+          <div className={styles["divider"]}></div>
+          <div className={styles["grid_7x1"]}>
+            {allDay?.map((day) => {
+              return <p>{day}</p>;
+            })}
+          </div>
+          <div className={styles["empty_schedule"]}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="64"
+              height="64"
+              fill="white"
+              viewBox="0 0 16 16"
+            >
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
+            </svg>
+            <h2>Din tidsplan er tom!</h2>
+            <div className={styles["line"]}></div>
+            <p>
+              Tilføj de bands du ikke vil gå glip af på <br></br> festivallen,så
+              du kan holde styr på dit program. <br></br>
+              <a href="/scheduledates">Tilføj favoritter</a>
+            </p>
+          </div>
         </div>
-
-        <p>No favorites chosen</p>
       </div>
     );
   } else {

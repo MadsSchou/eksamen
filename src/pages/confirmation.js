4521d8c19576e0444888cb1e268a6abf6d15704d
Christine Ramm <chri42mp@stud.kea.dk>
2023-05-29 16:29:37 +0200

    done

--- src/pages/confirmation.js
+++ src/pages/confirmation.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import Flow from "@/components/steps";
 import styles from "./Confirmation.module.css"; // Import custom CSS file
+import { db } from "@/firebase";
+import { StoreContext } from "@/context/ticketContext";
 
 export default function Confirmation() {
   const router = useRouter();
+
+  const data = useContext(StoreContext);
+
+  console.log(data);
+  useEffect(() => {
+    db.collection("allReservations").add(data);
+  }, []);
 
   return (
     <>
@@ -18,7 +27,9 @@ export default function Confirmation() {
       <div className={styles.confirmationPage}>
         <h1>Tak for dit køb!</h1>
         <p>Dit køb er bekræftet, find dine biletter på mail</p>
-        <button onClick={() => router.push("/")}>Gå tilbage til forsiden</button>
+        <button onClick={() => router.push("/")}>
+          Gå tilbage til forsiden
+        </button>
       </div>
     </>
   );

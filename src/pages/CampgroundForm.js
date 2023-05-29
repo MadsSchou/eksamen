fbf9201813eb2801e06b4befafc2a711648805e6
Daniel Nygaard <dani12h1@stud.kea.dk>
2023-05-29 10:38:04 +0200

    put update

--- src/pages/CampgroundForm.js
+++ src/pages/CampgroundForm.js
@@ -32,27 +32,30 @@ export default function Home({ data }) {
   };
 
   const addToBasket = () => {
-    dispatch({
-      action: "SET_AREA",
-      payload: {
-        area: selectedArea,
-        greenCamping,
-      },
-    });
-  };
+    if (selectedArea === 0)
+      dispatch({
+        action: "SET_AREA",
+        payload: {
+          area: selectedArea,
+          greenCamping,
+        },
+      });
 
-  // fetch("https://charm-pale-tub.glitch.me/reserve-spot", {
-  //   method: "PUT",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify(payload),
-  // })
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     const reservationId = data.id;
-  //     console.log("Reservation id:", reservationId);
-  //   });
+    const body = {
+      area: selectedArea,
+      amount: availableAmount,
+    };
+
+    fetch("https://charm-pale-tub.glitch.me/available-spots", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    })
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  };
 
   const handleAreaChange = (event) => {
     const selectedArea = event.target.value;
@@ -131,11 +134,11 @@ export default function Home({ data }) {
             {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>}
             <br></br>
             <div className={styles.centerButton}>
-              <Link href="/personalinfo">
-                <button onClick={addToBasket} disabled={isButtonDisabled}>
-                  Reserver
-                </button>
-              </Link>
+              {/* <Link href="/personalinfo"> */}
+              <button onClick={addToBasket} disabled={isButtonDisabled}>
+                Reserver
+              </button>
+              {/* </Link> */}
             </div>
           </div>
         </div>
